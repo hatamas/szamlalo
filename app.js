50dee41ead2404a2217d98081af1a76b3fe48fb7
Hajcsár Tamás <hajcsartamas@gmail.com>
2024-01-09 17:46:10 +0100

    kész2

--- app.js
+++ app.js
@@ -4,16 +4,34 @@ let start = document.getElementById("start");
 let val = document.getElementById("val");
 
 let ertek = 0
-val.innerHTML = ertek;
+val.value = ertek;
+
+function colorChange() {
+    if (ertek == 0) {
+        val.style.color = "black";
+    }
+    else if (ertek > 0) {
+        val.style.color = "green";
+    }
+    else {
+        val.style.color = "red";
+    }
+}
 
 plus.addEventListener("click", function() {
-    alert ("+");
+    ertek++;
+    val.value = ertek;
+    colorChange();
 })
 
 minus.addEventListener("click", function() {
-    alert ("-");
+    ertek--;
+    val.value = ertek;
+    colorChange();
 })
 
 start.addEventListener("click", function() {
-    alert ("start");
+    ertek = 0;
+    val.value = ertek;
+    colorChange();
 })
